97d82af626833d0665a25925b16f078a5a2dfd16
runru1030 <runru1030@gmail.com>
2025-07-01 21:37:19 +0900

    코드 수정

--- src/agents/main.py
+++ src/agents/main.py
@@ -2,7 +2,10 @@ import asyncio
 from langgraph.prebuilt import create_react_agent
 from src.agents.model import ModelID, get_model
 from dotenv import load_dotenv
-from src.tools.github import get_pull_requests, get_pull_request_changed_files
+from src.tools.github import (
+    get_pull_requests,
+    get_pull_request_changed_files,
+)
 from src.utils import pretty_print_messages
 
 load_dotenv()
@@ -11,13 +14,16 @@ load_dotenv()
 async def main():
     model = get_model(ModelID.OPENAI_GPT_41_MINI)
 
-    agent = create_react_agent(
-        model, [get_pull_requests, get_pull_request_changed_files]
-    )
+    tools = [
+        get_pull_requests,
+        get_pull_request_changed_files,
+    ]
+
+    agent = create_react_agent(model, tools)
 
     response = await agent.ainvoke(
         {
-            "messages": "runru1030 계정의 LLM_study 레포지토리에서 pull request 리스트를 조회해줘 그리고 그 pr들의 파일 변경 세부 내용을 요약해줘"
+            "messages": "HITS-AI 조직의 argocd 레포지토리에서 open 상태의 pull request 리스트를 조회해줘 그리고 그 pull request의 파일 변경 파일 코드도 조회해줘"
         }
     )
     pretty_print_messages(response)
